5dc9a453728806da80bf52acec9f5bef8f70306d
Josiah-Canada <josiahcanada@yahoo.com>
2022-06-12 21:12:44 -0400

    new notes

--- db/store.js
+++ db/store.js
@@ -9,13 +9,32 @@ const writeFile = util.promisify(fs.writeFile);
 class Store {
     // function that reads from the db.json file
     read() {
-        return readFile("db.json", "utf8")
+        return readFile("db/db.json", "utf8")
     }
     // function to write to the db.json file. that will take in some data. parameters
-    
+   write(note) {
+        console.log(note)
+        return writeFile("db/db.json", JSON.stringify(note));
+   }
     // function to get all the data from the db.json file. calls read function, and parse the notes.
-    
+   getNotes() {
+    return this.read().then(note => {
+        console.log(note)
+        let noteParse;
+
+        try {
+            noteParse = [].concat(JSON.parse(note));
+        } catch (err) {
+            noteParse = [];
+        }
+        return noteParse;
+    });
+   }
     // function to add to the notes. and use the write function already written. and pass on new notes as the parameter to the write function.
+    // get all notes and save the new notes to the list and write them all back to the file.
+    addNotes(note) {
+    return this.write(note)
+    }
     // function to removes notes. take in the id parameters and look for the note associated with the id and remove the note
     
     
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,19 @@ const router = require('express').Router();
 
 const store = require('../db/store')
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
    store.getNotes().then(e => {
     return res.json(e)
    }); 
 });
 
 // for the post route use req parameters to get the data for the body
+
+router.post('/notes', (req, res) => {
+   store.addNotes(req.body).then(e => {
+      return res.json(e)
+   });
+});
 // for the delete route you need the id from req.params.id
+
+module.exports = router
